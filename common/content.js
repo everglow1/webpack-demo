8046fbd8a7325718c7cb3489e8dbfaa883ba9ff9
weidian <youzhijun@weidian.com>
2020-01-20 16:48:04 +0800

    初始化

---
+++ common/content.js
@@ -0,0 +1,7 @@
+// export default function test() {
+// 	console.log(11111)
+// }
+
+module.exports = function test() {
+	console.log(22222)
+}
---
+++ index.js
@@ -0,0 +1,4 @@
+// import test from './common/content'
+const test = require('./common/content')
+
+test()
