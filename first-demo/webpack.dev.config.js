d0897c6aea8704b8b2a6a5b4a16187c5f754c97d
weidian <youzhijun@weidian.com>
2020-01-20 20:30:44 +0800

    完善代码

--- first-demo/webpack.dev.config.js
+++ first-demo/webpack.dev.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
 	entry: './src/index.js',
 	output: {
-		filename: 'bundle.js',
+		filename: 'bundledd.js',
 		path: path.resolve(__dirname + '/dist')
 	},
 }
