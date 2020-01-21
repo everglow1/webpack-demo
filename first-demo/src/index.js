7772c1233bb87357f2edce1ef233e634fdc27920
weidian <youzhijun@weidian.com>
2020-01-21 12:02:23 +0800

    file-loader

--- first-demo/src/index.js
+++ first-demo/src/index.js
@@ -1,4 +1,14 @@
 // import test from './common/content'
-const test = require('../../common/content')
+// const test = require('../../common/content')
+// const picture = require('./logo.png')
+// console.log(picture)
+// test()
+import avatar from './logo.png'
+import path from 'path'
+import header from './header.vue'
 
-test()
+let img = new Image()
+img.src = avatar;
+
+let root = document.getElementById('root')
+root.append(img)
--- first-demo/webpack.dev.config.js
+++ first-demo/webpack.dev.config.js
@@ -1,9 +1,33 @@
 const path = require('path')
 
 module.exports = {
-	entry: './src/index.js',
+	mode: 'production',
+	entry: {
+		main: './src/index.js'
+	},
+	module: {
+		rules: [{
+			test: /\.(jpg|png|gif)$/,
+			use: {
+				loader: 'file-loader',
+				options: {
+					name: '[name].[ext]',
+					outputPath: 'images/',
+					limit: 2048000,
+					publicPath: 'assets',
+					emitFile: true
+				}
+			}
+		},
+		{
+			test: /\.vue$/,
+			use: {
+				loader: 'file-loader'
+			}
+		}]
+	},
 	output: {
-		filename: 'bundledd.js',
+		filename: 'bundle.js',
 		path: path.resolve(__dirname + '/dist')
 	},
 }
