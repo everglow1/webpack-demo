90f5008aae54e772f3e337663246270858eb0943
weidian <youzhijun@weidian.com>
2020-01-22 23:39:30 +0800

    完善代码

--- first-demo/webpack.dev.config.js
+++ first-demo/webpack.dev.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
 	mode: 'production',
@@ -42,6 +44,12 @@ module.exports = {
 		}
 		]
 	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.html'
+		}),
+		new CleanWebpackPlugin()
+	],
 	output: {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname + '/dist')
